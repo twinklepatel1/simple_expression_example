c062737a1c326675ea655ec8c0ec18ee64438902
twinklepatel1 <85078184+twinklepatel1@users.noreply.github.com>
2021-05-31 00:58:42 +0530

    Add files via upload

---
+++ Simple Expression (1).py
@@ -0,0 +1,160 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Simple Expressions
+#  
+#  <h4>Boolean Evaluation</h4><br>
+#  Boolean expressions are created with keywords and ,or, not and Is .For Example:
+# 
+
+# In[1]:
+
+
+True and False
+
+
+# In[2]:
+
+
+True and True
+
+
+# In[3]:
+
+
+True and False
+
+
+# In[4]:
+
+
+not True
+
+
+# In[5]:
+
+
+True is True
+
+
+# In[6]:
+
+
+True is False
+
+
+# In[12]:
+
+
+'a' is 'a'
+
+
+# In[12]:
+
+
+True + True
+
+
+# In[13]:
+
+
+True + False
+
+
+# In[14]:
+
+
+False + False
+
+
+# In[15]:
+
+
+True - False
+
+
+# In[16]:
+
+
+False - False
+
+
+# In[17]:
+
+
+False - True
+
+
+# In[18]:
+
+
+10 == 10
+
+
+# In[19]:
+
+
+10 <= 10
+
+
+# In[20]:
+
+
+True and False and True
+
+
+# In[2]:
+
+
+a=10;
+b=20;
+
+
+# In[3]:
+
+
+a == b
+
+
+# In[4]:
+
+
+a != b
+
+
+# In[5]:
+
+
+a <= b
+
+
+# In[6]:
+
+
+a >= b
+
+
+# In[8]:
+
+
+a = 30
+b = 30
+
+
+# In[9]:
+
+
+a <= b
+
+
+# In[10]:
+
+
+a >= b
+
+
+# In[ ]:
+
+
+
+
